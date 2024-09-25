fa67856bd15fbdace89cab9244cdce93c138e817
Juan Camilo Cardona Pizarro <jucampuca@gmail.com>
2024-09-25 11:33:54 -0500

    Inicio del proyecto

---
+++ backend/rust-db-manager/target/debug/deps/rust_db_manager-d0997b6aace3ad5b.d
@@ -0,0 +1,5 @@
+/home/zombits/Rust/RUST-DB-MANAGER/backend/rust-db-manager/target/debug/deps/rust_db_manager-d0997b6aace3ad5b: src/main.rs
+
+/home/zombits/Rust/RUST-DB-MANAGER/backend/rust-db-manager/target/debug/deps/rust_db_manager-d0997b6aace3ad5b.d: src/main.rs
+
+src/main.rs:
---
+++ backend/rust-db-manager/target/debug/rust-db-manager.d
@@ -0,0 +1,1 @@
+/home/zombits/Rust/RUST-DB-MANAGER/backend/rust-db-manager/target/debug/rust-db-manager: /home/zombits/Rust/RUST-DB-MANAGER/backend/rust-db-manager/src/main.rs
